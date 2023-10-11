b36c1d57dd4e56594c55c4015f929ed931ec5abd
Nadeem1815 <nadeemf408@gmail.com>
2023-10-11 20:51:40 +0530

    Authenticaton middleware

---
+++ pkg/auth/middleware.go
@@ -0,0 +1,44 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/Nadeem1815/go-grpc-api-gateway/pkg/auth/pb"
+	"github.com/gin-gonic/gin"
+)
+
+type AuthMiddlewareConfig struct {
+	svc *ServiceClient
+}
+
+func InitAuthMiddleware(svc *ServiceClient) AuthMiddlewareConfig {
+	return AuthMiddlewareConfig{svc}
+
+}
+
+func (c *AuthMiddlewareConfig) AuthRequst(ctx *gin.Context) {
+	authorization := ctx.Request.Header.Get("authorization")
+	if authorization == "" {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+
+	}
+	token := strings.Split(authorization, "Bearer")
+
+	if len(token) < 2 {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+	res, err := c.svc.client.Validate(context.Background(), &pb.ValidateRequest{
+		Token: token[1],
+	})
+	if err != nil || res.Status != http.StatusOK {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	ctx.Set("userId", res.UserId)
+	ctx.Next()
+}
