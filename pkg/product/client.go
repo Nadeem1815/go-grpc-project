7ca6053ea8b10f8d9541e3ad32e635daa57a4282
Nadeem1815 <nadeemf408@gmail.com>
2023-10-14 16:04:21 +0530

    Product Microservice Client

--- pkg/product/client.go
+++ pkg/product/client.go
@@ -1,1 +1,25 @@
 package product
+
+import (
+	"fmt"
+
+	"github.com/Nadeem1815/go-grpc-api-gateway/pkg/config"
+	"github.com/Nadeem1815/go-grpc-api-gateway/pkg/product/pb"
+	"google.golang.org/grpc"
+)
+
+// we need to define the client to communicate with the Product Microservice.
+type ServiceClient struct {
+	client pb.ProductServiceClient
+}
+
+func InitServiceClient(c *config.Config) pb.ProductServiceClient {
+	// using WithInsecure() because no SSL running
+	cc, err := grpc.Dial(c.ProductSvcl, grpc.WithInsecure())
+
+	if err != nil {
+		fmt.Println("could not connect:", err)
+
+	}
+	return pb.NewProductServiceClient(cc)
+}
