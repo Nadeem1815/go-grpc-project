a4121f847a54a5f6a730441cd8f34f6fef803b3d
Nadeem1815 <nadeemf408@gmail.com>
2023-10-14 14:40:56 +0530

    order-routes

--- pkg/order/routes.go
+++ pkg/order/routes.go
@@ -1,1 +1,26 @@
 package order
+
+import (
+	"github.com/Nadeem1815/go-grpc-api-gateway/pkg/auth"
+	"github.com/Nadeem1815/go-grpc-api-gateway/pkg/config"
+	"github.com/Nadeem1815/go-grpc-api-gateway/pkg/order/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(r *gin.Engine, c *config.Config, authSvc *auth.ServiceClient) {
+	a := auth.InitAuthMiddleware(authSvc)
+
+	// svc := &ServiceClient{
+	// 	client: InitServiceClient(c),
+	// }
+	svc := &ServiceClient{
+		client: InitServiceClient(c),
+	}
+	routes := r.Group("/order")
+	routes.Use(a.AuthRequired)
+	routes.POST("/", svc.CreateOrder)
+}
+
+func (svc *ServiceClient) CreateOrder(ctx *gin.Context) {
+	routes.CreateOrder(ctx, svc.client)
+}
