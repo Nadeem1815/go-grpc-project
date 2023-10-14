7af3efb8de13a0e44405deb99a2771345c8f33de
Nadeem1815 <nadeemf408@gmail.com>
2023-10-14 16:31:10 +0530

    initalize Routes product

--- pkg/product/register.go
+++ pkg/product/register.go
@@ -1,1 +1,28 @@
 package product
+
+import (
+	"github.com/Nadeem1815/go-grpc-api-gateway/pkg/auth"
+	"github.com/Nadeem1815/go-grpc-api-gateway/pkg/config"
+	"github.com/Nadeem1815/go-grpc-api-gateway/pkg/product/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(r *gin.Engine, c *config.Config, authSvc *auth.ServiceClient) {
+	a := auth.InitAuthMiddleware(authSvc)
+
+	svc := &ServiceClient{
+		client: InitServiceClient(c),
+	}
+	routes := r.Group("/product")
+	routes.Use(a.AuthRequired)
+	routes.POST("/", svc.CreateProduct)
+	routes.GET("/:id", svc.FindOne)
+}
+
+func (svc *ServiceClient) FindOne(ctx *gin.Context) {
+	routes.FindOne(ctx, svc.client)
+}
+
+func (svc *ServiceClient) CreateProduct(ctx *gin.Context) {
+	routes.CreateProduct(ctx, svc.client)
+}
